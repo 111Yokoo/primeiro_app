7bbb2d55ace3fede21ab1336814a2cb17b6ed283
111Yokoo <yokoomatheus@gmail.com>
2024-02-22 10:28:05 -0300

    Componentização

--- App.js
+++ App.js
@@ -1,26 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView } from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView} from 'react-native';
 import { Feather } from '@expo/vector-icons';
 import { FontAwesome } from '@expo/vector-icons';
-import Logo from "../teste/assets/src/instagram-logo.png";
-import Pessoa from "../teste/assets/src/pessoa.webp";
-import Pessoa2 from "../teste/assets/src/pessoa2.webp";
+import Pessoa from "./src/assets/pessoa.webp";
+import Pessoa2 from "./src/assets/pessoa2.webp";
+import Header from "./src/components/Header";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.header}>
-        <StatusBar style="auto" />
-        <Image source={Logo} style={{height: 60, width: 200}}/>
-        <View style={styles.menuCaminhos}>
-          <TouchableOpacity>
-            <Feather name="heart" size={36} color="black"/>
-          </TouchableOpacity>
-          <TouchableOpacity>
-            <FontAwesome name="location-arrow" size={36} color="black" />
-          </TouchableOpacity>
-        </View>
-      </View>
+      <Header/>
       <View>
         <ScrollView showsHorizontalScrollIndicator={false} contentContainerStyle={{gap: 8}} horizontal style={{flexDirection: "row", gap: 16}}>
           <Image style={{ width: 100, height: 100, borderRadius: 50}} source={Pessoa}/>
@@ -41,14 +30,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     marginVertical: 35,
     marginHorizontal: 16,
-  },
-  header: {
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  },
-  menuCaminhos: {
-    flexDirection: 'row',
-    gap: 10,
-    alignItems: 'center',
   },
 });
